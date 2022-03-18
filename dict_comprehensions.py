38fcc1f51674300abda263631cee952ff037f1f4
lucioled <lucioled01@gmail.com>
2022-03-18 16:58:56 -0300

    Remove unnecesary

--- dict_comprehensions.py
+++
@@ -1,6 +0,0 @@
-from cmath import sqrt
-import math
-if __name__ == '__main__':
-    my_object = {i: round(i**0.5, 2) for i in range(1, 11)}
-
-    print(my_object)
--- list_comprehensions.py
+++
@@ -1,5 +0,0 @@
-if __name__ == '__main__':
-    my_list = [i for i in range(1, 99999) if i %
-               4 == 0 if i % 6 == 0 if i % 9 == 0]
-
-    print(my_list)
